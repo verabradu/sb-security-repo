dbb33ade3be088189a9e1f0561cd43002aa63b8b
verabradu <verabradu92@gmail.com>
2025-05-21 10:52:28 -0500

    Initial commit

---
+++ src/main/java/com/example/My/Secret/Project/MySecretProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.My.Secret.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySecretProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySecretProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/My/Secret/Project/MySecretProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.My.Secret.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecretProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
